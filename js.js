a2dc828167473fac3e7b65ad6e19244685dea027
andresrodaas <69936850+andresrodaas@users.noreply.github.com>
2021-03-30 23:07:13 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,25 @@
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarMenu = document.querySelector(".navbar ul");
+const navbarLinks = document.querySelectorAll(".navbar a");
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick() {
+  navbarToggler.classList.toggle("open-navbar-toggler");
+  navbarMenu.classList.toggle("open");
+}
+
+navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
+
+function navbarLinkClick() {
+  if(navbarMenu.classList.contains("open")) {
+    navbarToggler.click();
+  }
+}
+
+window.addEventListener('scroll' , function(){
+
+let nav = document.querySelector('.navbar ')
+ nav.classList.toggle('scrolling-active', window.scrollY >0 ); 
+
+}); 
